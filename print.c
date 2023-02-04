0bb04d7954ff66c570c71740eb771cc20371b2e5
Aditya4769 <114800003+Aditya4769@users.noreply.github.com>
2023-02-04 10:16:36 +0530

    C

---
+++ print.c
@@ -0,0 +1,60 @@
+//#include<stdio.h>
+//void main(){
+//
+//	printf("Hello Everyone");
+//}
+//#include<stdio.h>
+//void main(){
+//	int a = 9;
+//	int b = 4;
+//    
+//    int c = a+b;
+//    printf("a+b = %d \n",c);
+//}
+
+#include<stdio.h>
+void main(){
+	int age;
+	printf("Enter Your age: \n");
+	scanf("%d",&age);
+	if (age>=18){
+		printf("Eligible");
+	}
+
+	else{
+		printf("Not-Eligible");
+		}
+}
+
+//#include<stdio.h>
+//void main(){
+//	int a;
+//	int b;
+//	int c;
+//	printf("Enter your First Number: \n");
+//	printf("Enter your Second Number: \n");
+//	printf("Enter Your Opertors you want to operates: \n");
+//	scanf("%d",&a);
+//	scanf("%d",&b);
+//	scanf("%d",&c);
+//	
+//	if (c== 1){
+//		print("Addition of Your No is: %d \n",a+b);
+//	}
+//	else if (c== -){
+//		print("Subtraction of Your No is: %d \n",a-b);
+//	}
+//	else if (c== *){
+//		print("Multiplication of Your No is: %d \n",a*b);
+//	}
+//	else if (c== **){
+//		print("Power of numbers of Your No is: %d \n",a**b);
+//	}
+//	else if (c== /){
+//		print("Divide of Your No is: %d \n",a/b);
+//	}
+//	else{
+//		print("Mode of Your No is: %d \n",a%b);
+//	}
+//	
+//}
