41d832355006db1d9bcd5d413f43dcb285fa45b1
Aditya4769 <114800003+Aditya4769@users.noreply.github.com>
2023-02-15 15:14:11 +0530

    C

--- If_else.c
+++ If_else.c
@@ -1,2 +1,45 @@
-#include<stdio.h>
+//#include<stdio.h>
+//void main(){
+//	int a;
+//	printf("Enter Your Number: ");
+//	scanf("%d",&a);
+//	if (a>0){
+//		printf("%d is Positive Number",a);}
+//	else if(a==0){
+//		printf("Value is Zero");
+//		}
+//	
+//	else{
+//		printf("%d is negative Number",a);
+//	}
 
+//a>0?printf("+ve"):printf("-ve");
+//}
+
+
+
+
+
+//SWITCH CASE:
+#include <stdio.h>
+void main(){
+	char ch;
+	scanf("%c",&ch);
+	switch(ch){
+		case 'm':
+			printf("Monday");
+			break;
+		case 't':
+			printf("Tuesday");
+			break;
+		case 'w':
+			printf("Wednesday");
+			break;
+		case 'T':
+			printf("Thursday");
+			break;
+		default:
+			printf("Invalid");
+	}
+	
+}
--- calculator.c
+++ calculator.c
@@ -37,29 +37,165 @@
 //}
 
 
-#include<stdio.h>
-void main(){
-	int a;
-	printf("Enter Your First Number: \n");
-	scanf("%d",&a);
-	int b;
-	printf("Enter Your Second Number: \n");
-	scanf("%d",&b);
-	int c;
-	printf("Enter Your Third Number: \n");
-	scanf("%d",&c);
-	if (a>b && a>c){
-		printf("%d",a);
-	}
-	else if(b>a && b>c){
-		printf("%d",b);
-	}
-	else{
-		printf("%d",c);
-	}
-}
+//#include<stdio.h>
+//void main(){
+//	int a;
+//	printf("Enter Your First Number: \n");
+//	scanf("%d",&a);
+//	int b;
+//	printf("Enter Your Second Number: \n");
+//	scanf("%d",&b);
+//	int c;
+//	printf("Enter Your Third Number: \n");
+//	scanf("%d",&c);
+//	if (a>b && a>c){
+//		printf("%d",a);
+//	}
+//	else if(b>a && b>c){
+//		printf("%d",b);
+//	}
+//	else{
+//		printf("%d",c);
+//	}
+//}
 
 
 
 
 
+
+//1.Even or Odd
+//2.take the input from user and check ,the number is divisible by 2 and 4
+//3.Find the greates number among the three input
+//4.aritmetic opration take two input for no and in the case
+//5.write a program to get 2 largedt number among the three numbers
+
+
+
+//1.
+//#include <stdio.h>
+//void main(){
+//	int a;
+//	scanf("%d",&a);
+//	a%2==0?printf("Even"):printf("Odd");
+//}
+
+
+
+//2.
+//#include<stdio.h>
+//void main(){
+//	int a;
+//	printf("Enter Your Number: ");
+//	scanf("%d",&a);
+//	if (a%2==0 && a%4==0){
+//		printf("Yes %d is divisible by 2 and 4",a);
+//	
+//	}
+//	else{
+//		printf("It is Not Possible");
+//	}
+//}
+
+
+
+//3.
+//#include<stdio.h>
+//void main(){
+//	int a;
+//	printf("Enter Your First Number: \n");
+//	scanf("%d",&a);
+//	int b;
+//	printf("Enter Your Second Number: \n");
+//	scanf("%d",&b);
+//	int c;
+//	printf("Enter Your Third Number: \n");
+//	scanf("%d",&c);
+//	if (a>b && a>c){
+//		printf("%d",a);
+//	}
+//	else if(b>a && b>c){
+//		printf("%d",b);
+//	}
+//	else{
+//		printf("%d",c);
+//	}
+//}
+
+
+
+
+
+//4.
+//#include<stdio.h>
+//void main(){
+//	int a;
+//	printf("Enter Your First Number: \n");
+//	scanf("%d",&a);
+//	int b;
+//	printf("Enter Your Second Number: \n");
+//	scanf("%d",&b);
+//	int c;
+//	printf("Enter Your Third Number: \n");
+//	scanf("%d",&c);
+//	if (a>b && a>c){
+//		if (b>c){
+//			printf("%d",b);
+//		}
+//		else{
+//			printf("%d",c);
+//		}
+//	}
+//	else if (b>a && b>c){
+//		if (a>c){
+//			printf("%d",a);
+//		}
+//		else{
+//			printf("%d",c);
+//		}
+//	}
+//	else if (c>a && c>b){
+//		if (a>c){
+//			printf("%d",a);
+//		}
+//		else{
+//			printf("%d",b);
+//		}
+//	}
+//	else{
+//		printf("Invalid");
+//	}
+//}
+
+
+//5.
+//#include<stdio.h>
+//void main(){
+//	char op;
+//	printf("Enter Your oprators(+,-,*,/,%)");
+//	scanf("%c",&op);
+//	int a;
+//	printf("Enter Your Number: ");
+//	scanf("%d",&a);
+//	int b;
+//	printf("Enter Your Number: ");
+//	scanf("%d",&b);
+//	
+//	switch(op){
+//		case '+':
+//			printf("%d",a+b);
+//			break;
+//		case '-':
+//			printf("%d",a-b);
+//			break;
+//		case '*':
+//			printf("%d",a*b);
+//			break;
+//		case '/':
+//			printf("%d",a/b);
+//			break;
+//
+//		default:
+//			printf("Invalid");
+//	}
+//}
