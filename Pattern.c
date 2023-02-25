9c38e6dd952009ef029c32e63fce32a1265debba
Aditya4769 <114800003+Aditya4769@users.noreply.github.com>
2023-02-25 10:05:19 +0530

    C

--- Pattern.c
+++ Pattern.c
@@ -1,19 +1,19 @@
-//#include <stdio.h>
-//
-//int main()
-//{
-//    int i, j;
-//    for(i=1;i<=4;i++)
-//    {
-//        for(j=1;j<=i;j++)
-//        {
-//            printf("%d ",j);
-//        }
-//        printf("\n");
-//    }
-//
-//    return 0;
-//}
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+    for(i=1;i<=4;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
 
 
@@ -56,15 +56,27 @@
 //}
 
 
+//
+//#include<stdio.h>
+//int main(){
+//	int n=10;
+//	while(n>0){
+//		printf("%d",n);
+//		if (n%2==1)
+//			continue;
+//		n= n-1;
+//		
+//	}
+//}
 
-#include<stdio.h>
-int main(){
-	int n=10;
-	while(n>0){
-		printf("%d",n);
-		if (n%2==1)
-			continue;
-		n= n-1;
-		
-	}
-}
+
+
+
+//#include<stdio.h>
+//void main(){
+//	printf("%5d\n",1);
+//	printf("%5d\n",12);
+//	printf("%5d\n",123);
+//	printf("%5d\n",1234);
+//	printf("%5d\n",12345);
+//}
