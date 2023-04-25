06f6bbd12e7c71ce5c0658c1b6356517c4de991e
Aditya4769 <114800003+Aditya4769@users.noreply.github.com>
2023-04-25 12:24:48 +0530

    C

--- StringLecture2.c
+++ StringLecture2.c
@@ -1,1 +1,100 @@
 #include<stdio.h>
+#include<string.h>
+//int main(){
+//	char ch[]="Hello123$";
+//	char ch1[10];
+//	strcpy(ch1,ch);
+//	printf("%s",ch1);
+//}
+
+
+//int main(){
+//	char ch[]="Hello123$";
+//	char ch1[10];
+//	strncpy(ch1,ch,3);
+//	printf("%s",ch1);
+//}
+
+
+
+
+//int main(){
+//	char ch[]="Hello123$";
+//	char ch1[10];
+//	strncpy(ch1,ch,3);
+//	printf("%s",ch1);
+//	printf("\n%s",strcat(ch1,ch));
+//}
+
+
+
+//int main(){
+//	char ch[]="Hello123$";
+//	char ch1[10];
+//	strncpy(ch1,ch,3);
+//	printf("%s",ch1);
+//	printf("\n%s",strncat(ch1,ch,3));
+//}
+
+
+
+
+
+
+
+//int main(){
+//	char ch[]="Hello123$";
+//	char ch1[10];
+//	char ch2[10];
+//	strncpy(ch1,ch,3);
+//	printf("%s",ch1);
+//	strcpy(ch2,strcat(ch1,ch));
+//	printf("\n%s",ch2);
+//}
+
+
+
+
+
+
+//int main(){
+//	char ch[]="abcd";
+//	char ch1[10];
+//	printf("%s\n",strncpy(ch1,ch,2));
+//	printf("%d",strcmp(ch1,ch));
+//	}
+
+
+
+
+//int main(){
+//	char ch[]="abcd";
+//	char ch1[10];
+//	printf("%s\n",strcpy(ch1,ch));
+//	printf("%d",strcmp(ch1,ch));
+//	}
+
+
+
+
+//int main(){
+//	char ch[]="abcd";
+//	char ch1[10]="aditya";
+//	printf("%d",strcmp(ch1,ch));
+//	}
+
+
+
+
+
+//int main(){
+//	char ch[]="abcd";
+//	char ch1[]="wert";
+//	printf("%d",strcmp(ch,ch1));
+//	}
+
+
+
+
+
+
